6d149bd404d9c7a4fbfdc9979f7678f49ec88a3a
Majer, Norbert <norbert.majer@kingfisher.com>
2023-04-02 19:04:00 +0100

    update username

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 const client = new Client({
-  user: 'azotamiota',
+  user: process.env.USER,
   host: 'localhost',
   database: 'mydatabase',
   password: process.env.PSQL_PWD,
